446c2b57688bca5416a87e462bb7d4c9c29a91bf
Marius Avram <marius.avram1309@yahoo.ro>
2014-09-25 15:04:20 +0300

    Added API for user registration and user get
    The user get can be used to view the page of the user.
    
    Signed-off-by: Marius Avram <marius.avram1309@yahoo.ro>

--- Server/server/server.js
+++ Server/server/server.js
@@ -6,6 +6,7 @@ var mongoose        = restful.mongoose;
 var methodOverride  = require('method-override');
 var path            = require('path');
 var router          = express.Router();
+var validator       = require('validator');
 var models          = require('./models');
 
 GLOBAL.app = express();
@@ -21,6 +22,7 @@ var port = 8080;
 
 // connect to local database
 mongoose.connect('mongodb://localhost/ape');
+var User            = mongoose.model('User');
 var Challenge       = mongoose.model('Challenge');
 var Reply           = mongoose.model('Reply');
 
@@ -29,7 +31,7 @@ var Reply           = mongoose.model('Reply');
 // main feed containg replies to latest challenge
 router.route('/feed')
     .get(function(req, res) {
-        // find lastest challenge
+        // find latest challenge
         Challenge.findOne().sort('-date').exec(
             function(err, challenge) {
                 // find replies to it and return them
@@ -41,6 +43,63 @@ router.route('/feed')
                 });
         });
     });
+
+// sign up new user
+router.route('/signup')
+    .post(function(req, res) {
+        // check required fields are filled
+        if (!req.body.name || !req.body.email || !req.body.password) {
+            res.json({result : 'failure', errors : ['Please complete all fields.']});
+            return;
+        }
+        // check username uniqueness
+        User.findOne({ name : req.body.name}, function(err, user_found) {
+            var errors = [];
+            if (user_found)
+                errors.push('Username already exists.');
+            // check email is valid
+            if (!validator.isEmail(req.body.email)) {
+                errors.push('Invalid email address.');
+            }
+
+            // check email uniqueness
+            User.findOne({ email : req.body.email }, function(err, user_found) {
+                if (user_found)
+                    errors.push('Email address is already in use.');
+
+                // return error if validation failed
+                if (errors.length != 0) {
+                    res.json({result : 'failure', errors : errors});
+                    return;
+                }
+                // save the object
+                var user = new User();
+                user.name = req.body.name;
+                user.password = req.body.password;
+                user.email = req.body.email;
+
+                user.save(function(err) {
+                    if (err)
+                        res.send(err);
+                    res.json({result : 'success'});
+                });
+            });
+        });
+    });
+
+// get user info
+router.route('/user/:name')
+
+    .get(function(req, res) {
+        // do not expose password and email
+        User.findOne({name : req.params.name}, '-password -email', function(err, user) {
+            if (err)
+                res.send(err);
+            res.json(user);
+        });
+    });
+
+
 
 // register routes -------------------------------
 // all of our routes will be prefixed with /api
