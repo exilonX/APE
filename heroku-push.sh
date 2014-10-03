47a25a8fdd1b0ddcbfc22abaa6588bec46cb119b
Marius Avram <marius.avram1309@yahoo.ro>
2014-10-04 02:36:04 +0300

    heroku push automatically sets debug to false
    Signed-off-by: Marius Avram <marius.avram1309@yahoo.ro>

--- heroku-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# will push the Server subdirectory to the heroku platform
-# does local branch switching if needed
-
-# author: Marius Avram 2014
-
-deployment_branch="develop" # change here if the case
-git push heroku `git subtree split --prefix Server $deployment_branch`:master --force
